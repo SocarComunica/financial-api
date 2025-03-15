03cdb7267e7d89727cc9453f28b00ec737e4f7e7
Oscar Nicolas Patarroyo Laverde <nicolaspatarroyo@hotmail.com>
2025-03-15 09:15:00 -0500

    add: password fields for user creates

--- internal/adapters/producer/http/request/users.go
+++ internal/adapters/producer/http/request/users.go
@@ -3,4 +3,5 @@ package request
 type CreateUser struct {
 	Username string `json:"username" validate:"required"`
 	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
 }
