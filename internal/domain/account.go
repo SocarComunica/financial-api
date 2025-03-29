90289837d69ac4fe228b65cdd07a738058ec8b3e
Oscar Nicolas Patarroyo Laverde <nicolaspatarroyo@hotmail.com>
2025-03-29 15:04:08 -0500

    add: new field to relate user with accounts

--- internal/domain/account.go
+++ internal/domain/account.go
@@ -4,9 +4,10 @@ import "gorm.io/gorm"
 
 type Account struct {
 	gorm.Model
-	Name           string  `json:"name"`
-	Type           string  `json:"type"`
-	Balance        float64 `json:"balance"`
-	InitialBalance float64 `json:"initial_balance"`
-	UserID         uint    `json:"user_id"`
+	Name           string  `json:"name" gorm:"not null"`
+	Type           string  `json:"type" gorm:"not null"`
+	Balance        float64 `json:"balance" gorm:"not null;default:0"`
+	InitialBalance float64 `json:"initial_balance" gorm:"not null;default:0"`
+	UserID         uint    `json:"user_id" gorm:"not null"`
+	User           User    `json:"user" gorm:"foreignKey:UserID;references:ID"`
 }
