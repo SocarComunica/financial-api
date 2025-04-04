0b3d875f5b32722139e84fa0dfcd1f66b6b7b289
Oscar Nicolas Patarroyo Laverde <nicolaspatarroyo@hotmail.com>
2025-04-03 19:30:28 -0500

    add: Tx control over transaction on the DB

--- internal/application/transaction/service.go
+++ internal/application/transaction/service.go
@@ -3,7 +3,6 @@ package transaction
 import (
 	"errors"
 
-	"github.com/labstack/gommon/log"
 	"github.com/socarcomunica/financial-api/common"
 	"github.com/socarcomunica/financial-api/internal/adapters/producer/http/request"
 	"github.com/socarcomunica/financial-api/internal/domain"
@@ -16,7 +15,7 @@ const (
 type TransactionsDatabase interface {
 	AddTransaction(model *domain.Transaction) (*domain.Transaction, error)
 	GetAccount(id uint) (*domain.Account, error)
-	UpdateAccountBalance(account *domain.Account) error
+	UpdateAccountBalances(origin *domain.Account, destination *domain.Account) error
 	GetTransactionsByAccount(accountID uint, offset int) ([]*domain.Transaction, error)
 }
 
@@ -31,13 +30,12 @@ func NewTransactionService(database TransactionsDatabase) *Service {
 }
 
 func (t *Service) AddTransaction(request request.CreateTransaction) (*domain.Transaction, error) {
-
-	account, err := t.Database.GetAccount(request.OriginID)
+	origin, err := t.Database.GetAccount(request.OriginID)
 	if err != nil {
 		return nil, errors.New(AddTransactionError + err.Error())
 	}
 
-	if account.Balance < request.Amount && request.Type == common.TransactionTypeDebit {
+	if origin.Balance < request.Amount && request.Type == common.TransactionTypeDebit {
 		return nil, errors.New(AddTransactionError + "insufficient funds")
 	}
 
@@ -55,7 +53,7 @@ func (t *Service) AddTransaction(request request.CreateTransaction) (*domain.Tra
 		}(),
 		Type:          request.Type,
 		OriginID:      request.OriginID,
-		Origin:        *account,
+		Origin:        *origin,
 		DestinationID: request.DestinationID,
 	}
 
@@ -68,7 +66,8 @@ func (t *Service) AddTransaction(request request.CreateTransaction) (*domain.Tra
 		transactionModel.Destination = destination
 	}
 
-	if err := t.updateOriginAndDestinationBalance(request, account, destination); err != nil {
+	// Update balances in a single transaction
+	if err := t.updateBalances(request, origin, destination); err != nil {
 		return nil, err
 	}
 
@@ -80,23 +79,27 @@ func (t *Service) AddTransaction(request request.CreateTransaction) (*domain.Tra
 	return transaction, nil
 }
 
-func (t *Service) updateOriginAndDestinationBalance(request request.CreateTransaction, account *domain.Account, destination *domain.Account) error {
+func (t *Service) updateBalances(request request.CreateTransaction, origin *domain.Account, destination *domain.Account) error {
 	switch request.Type {
 	case common.TransactionTypeCredit:
-		account.Balance += request.Amount
-	case common.TransactionTypeDebit:
-		account.Balance -= request.Amount
-	case common.TransactionTypeTransfer:
-		account.Balance -= request.Amount
-		destination.Balance += request.Amount
-		if err := t.Database.UpdateAccountBalance(destination); err != nil {
-			log.Error("error updating destination account balance: ", err)
+		origin.Balance += request.Amount
+		if destination != nil {
+			destination.Balance -= request.Amount
 		}
+	case common.TransactionTypeDebit:
+		origin.Balance -= request.Amount
+		if destination != nil {
+			destination.Balance += request.Amount
+		}
+	case common.TransactionTypeTransfer:
+		if destination == nil {
+			return errors.New("destination account is required for transfer transactions")
+		}
+		origin.Balance -= request.Amount
+		destination.Balance += request.Amount
 	}
-	if err := t.Database.UpdateAccountBalance(account); err != nil {
-		log.Error("error updating origin account balance: ", err)
-	}
-	return nil
+
+	return t.Database.UpdateAccountBalances(origin, destination)
 }
 
 func (t *Service) GetTransactionsByAccount(accountID uint, offset int) ([]*domain.Transaction, error) {
